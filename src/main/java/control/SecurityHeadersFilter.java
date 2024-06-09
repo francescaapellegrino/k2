8e0fff66139168ec456bb4a6d731c35d15c61512
franc <franc@LAPTOP-Francesca>
2024-06-09 21:50:46 +0200

    id-5 unresolved

---
+++ src/main/java/control/SecurityHeadersFilter.java
@@ -0,0 +1,32 @@
+package control;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+
+public class SecurityHeadersFilter {
+
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+
+        httpResponse.setHeader("X-Content-Type-Options", "nosniff");
+        httpResponse.setHeader("X-XSS-Protection", "1; mode=block");
+        httpResponse.setHeader("Strict-Transport-Security", "max-age=31536000; includeSubDomains");
+        httpResponse.setHeader("Content-Security-Policy", "default-src 'self'");
+        httpResponse.setHeader("Referrer-Policy", "no-referrer");
+        httpResponse.setHeader("X-Frame-Options", "DENY");
+
+        // Continua la catena dei filtri
+        chain.doFilter(request, response);
+    }
+	
+}
